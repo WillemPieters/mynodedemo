f4ae621a3c5a63a3c5b8b142d42975346ab32c3b
WillemPieters <33497635+WillemPieters@users.noreply.github.com>
2017-11-16 11:52:42 +0200

    Add files via upload

---
+++ routes/places.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('places', { title: 'Places I Plan on Visiting',hasPlaces:true,
+    places:{1:{name:"Japan",url:"http://en.wikipedia.org/wiki/Japan"},
+            2:{name:"Scotland",url:"http://en.wikipedia.org/wiki/Scotland"},
+            3:{name:"Brazil",url:"http://en.wikipedia.org/wiki/Brazil"},
+            4:{name:"South Africa",url:"http://en.wikipedia.org/wiki/South_Africa"}
+          }});
+});
+
+module.exports = router;
